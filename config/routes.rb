741a94deb8fe62cd770bbdd1af7cb0c5ddca1fc6
aegerscovitae <garrettstott@gmail.com>
2015-06-12 23:18:44 +0000

    created chefs resources and add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
     end
   end
   
+  
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
+  
+  get '/login', to: "login#new"
+  post '/login', to: "login#create"
+  get '/logout', to: "login#destroy"
    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528014803) do
+ActiveRecord::Schema.define(version: 20150607002553) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "likes", force: true do |t|
