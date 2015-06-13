de10d52717f036bea29b65112e48d1cc12f5d272
aegerscovitae <garrettstott@gmail.com>
2015-06-13 00:08:47 +0000

    handle image upload in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,13 +4,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   
   include CarrierWave::MiniMagick
   process resize_to_limit: [600, 600]
-  storage :file 
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   attr_reader :attr_names
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+   storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
